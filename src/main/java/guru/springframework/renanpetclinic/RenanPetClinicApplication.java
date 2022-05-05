faea0ae78a5444d3da0388424951332ad4c5fe2d
RenanMacielLuna <renanluna09@gmail.com>
2022-05-05 11:14:43 -0300

    Initial Project Add

---
+++ src/main/java/guru/springframework/renanpetclinic/RenanPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.renanpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenanPetClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RenanPetClinicApplication.class, args);
+  }
+
+}
---
+++ src/test/java/guru/springframework/renanpetclinic/RenanPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.renanpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenanPetClinicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
